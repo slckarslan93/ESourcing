af6f2dc187ddd9d285d22008e3bc8c8b6de5019a
Selcuk ARSLAN <slckarslan93@gmail.com>
2022-10-02 21:12:09 +0300

    Esourcing

---
+++ Esourcing.Sourcing/Controllers/AuctionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Esourcing.Sourcing.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuctionController : ControllerBase
+    {
+       
+    }
+}
